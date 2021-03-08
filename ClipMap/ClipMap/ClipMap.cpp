098e93ff3c0eed6be4a5c2f9a395d877617d57d5
vanCopper <vancopper@foxmail.com>
2021-03-08 14:14:12 +0800

    rename files.

--- ClipMap/ClipMap/ClipMap.cpp
+++ ClipMap/ClipMap/ClipMap.cpp
@@ -72,7 +72,7 @@ int main(void)
     glBindVertexArray(VertexArrayID);
 
     // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders("Shaders/SimpleVertexShader.vertexshader", "Shaders/SimpleFragmentShader.fragmentshader");
+    GLuint programID = LoadShaders("Shaders/SimpleVertexShader.vs", "Shaders/SimpleFragmentShader.frag");
 
     // Get a handle for our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -91,17 +91,17 @@ int main(void)
    
 
     // Projection matrix : 45бу Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-    // Camera matrix
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
-        glm::vec3(0, 0, 0), // and looks at the origin
-        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-    // Model matrix : an identity matrix (model will be at the origin)
-    glm::mat4 Model = glm::mat4(1.0f);
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    //glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    //// Camera matrix
+    //glm::mat4 View = glm::lookAt(
+    //    glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
+    //    glm::vec3(0, 0, 0), // and looks at the origin
+    //    glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+    //);
+    //// Model matrix : an identity matrix (model will be at the origin)
+    //glm::mat4 Model = glm::mat4(1.0f);
+    //// Our ModelViewProjection : multiplication of our 3 matrices
+    //glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
     do {
 
@@ -111,11 +111,11 @@ int main(void)
         glUseProgram(programID);
 
         // Compute the MVP matrix from keyboard and mouse input
-        /*computeMatricesFromInputs();
+        computeMatricesFromInputs();
         glm::mat4 ProjecttionMatrix = getProjectionMatrix();
         glm::mat4 ViewMatrix = getViewMatrix();
         glm::mat4 ModelMatrix = glm::mat4(1.0);
-        glm::mat4 MVP = ProjecttionMatrix * ViewMatrix * ModelMatrix;*/
+        glm::mat4 MVP = ProjecttionMatrix * ViewMatrix * ModelMatrix;
 
         // Send our transformation to the currently bound shader, 
         // in the "MVP" uniform
@@ -123,7 +123,7 @@ int main(void)
 
         // 1rst attribute buffer : vertices
         glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        //glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
             0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
             3,                  // size
--- ClipMap/ClipMap/Common/Controls.cpp
+++ ClipMap/ClipMap/Common/Controls.cpp
@@ -21,7 +21,7 @@ glm::mat4 getProjectionMatrix() {
 
 
 // Initial position : on +Z
-glm::vec3 position = glm::vec3(0, 0, 5);
+glm::vec3 position = glm::vec3(4, 3, -3);
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // Initial vertical angle : none
@@ -29,10 +29,8 @@ float verticalAngle = 0.0f;
 // Initial Field of View
 float initialFoV = 45.0f;
 
-float speed = 1.0f; // 3 units / second
+float speed = .5f; // 3 units / second
 float mouseSpeed = 0.005f;
-
-
 
 void computeMatricesFromInputs() {
 
@@ -45,14 +43,14 @@ void computeMatricesFromInputs() {
 
     // Get mouse position
     double xpos, ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
+    //glfwGetCursorPos(window, &xpos, &ypos);
 
     // Reset mouse position for next frame
-    glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+    //glfwSetCursorPos(window, 1024 / 2, 768 / 2);
 
     // Compute new orientation
-    horizontalAngle += mouseSpeed * float(1024 / 2 - xpos);
-    verticalAngle += mouseSpeed * float(768 / 2 - ypos);
+   /* horizontalAngle += mouseSpeed * float(1024 / 2 - xpos);
+    verticalAngle += mouseSpeed * float(768 / 2 - ypos);*/
 
     // Direction : Spherical coordinates to Cartesian coordinates conversion
     glm::vec3 direction(
@@ -72,19 +70,19 @@ void computeMatricesFromInputs() {
     glm::vec3 up = glm::cross(right, direction);
 
     // Move forward
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
         position += direction * deltaTime * speed;
     }
     // Move backward
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
         position -= direction * deltaTime * speed;
     }
     // Strafe right
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
         position += right * deltaTime * speed;
     }
     // Strafe left
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
         position -= right * deltaTime * speed;
     }
 
@@ -95,7 +93,8 @@ void computeMatricesFromInputs() {
     // Camera matrix
     ViewMatrix = glm::lookAt(
         position,           // Camera is here
-        position + direction, // and looks here : at the same position, plus "direction"
+        //position + direction, // and looks here : at the same position, plus "direction"
+        glm::vec3(0,0,0),
         up                  // Head is up (set to 0,-1,0 to look upside-down)
     );
 
